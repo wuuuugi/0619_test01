74a75e32c2e5828ae7b798759b5c6c90c913de4d
tj-bu-706-07 <tj-bu-706-07@DESKTOP-S8NCJVI>
2023-06-19 11:40:44 +0900

    Test commit

---
+++ 01_javaPro/src/ch01/Git_test.java
@@ -0,0 +1,16 @@
+package ch01;
+
+public class Git_test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int nnan = 0;
+		int ndan = 0;
+		for (nnan = 2; nnan < 10; nnan++)
+			for (ndan = 1; ndan < 10; ndan++) {
+				System.out.println(nnan + " * " + ndan + " = " + nnan * ndan);
+		}
+
+	}
+
+}
